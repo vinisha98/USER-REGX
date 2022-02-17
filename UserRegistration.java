ce52cbfd141c1c0733615689faa5ae040ad31219
unknown <vinishakr9@gmail.com>
2022-02-17 21:14:51 +0530

    Added last name regex code

--- UserRegistration.java
+++ UserRegistration.java
@@ -4,9 +4,14 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
+	
 	public static void main(String[] args) {
+			firstName();
+			lastName();
+		}
+	
+	public static void firstName() {
 		Scanner sc = new Scanner(System.in);
-		
 		System.out.println("Enter first name: ");
 		String firstName = sc.next();
 		boolean isFirstName = Pattern.matches("[A-Z]+[a-z]{2,}", firstName);
@@ -17,7 +22,19 @@ public class UserRegistration {
 		else {
 			System.out.println("Invalid first name");
 		}
-
 	}
-
+	
+	public static void lastName() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("\nEnter last name: ");
+		String lastName = sc.next();
+		boolean isLastName = Pattern.matches("[A-Z]+[a-z]{2,}", lastName);
+		
+		if(isLastName) {
+			System.out.println("Valid last name");
+		}	
+		else {
+			System.out.println("Invalid last name");
+		}
+	}
 }
